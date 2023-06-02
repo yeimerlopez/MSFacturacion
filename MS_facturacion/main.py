bea00cc4e1c60a09c40a7b78320e63e06272f28a
yeimer lopez <yeimerlopez1216@gmail.com>
2023-06-01 21:21:43 -0500

    proyecto funcionando hasta la actualizacion de archivo main con loadfileconfig

--- MS_facturacion/main.py
+++ MS_facturacion/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
 
 
-# Press the green button in the gutter to run the script.
+
+#print("Mobile planet")
+
+app = Flask(__name__)
+cors = CORS(app)
+
+#función para leer el archivo de configuración
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
